8a6f2418912ee7aa0757a137038f453119df8d44
EzequielBW <ezebw2@gmail.com>
2021-02-13 00:06:04 -0300

    Update main.py
    Remove test-phase comments

--- app/main.py
+++ app/main.py
@@ -9,13 +9,6 @@ from flask_wtf import FlaskForm
 from wtforms import SelectField, SubmitField
 from wtforms.validators import DataRequired
 
-
-#TODO - REPLACE CREDENTIALS WITH .env variables
-#TODO - DATABASE IS OFF BY ONE DATE (e.g entry for November 26 has data for November 27)
-
-#labels = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC']
-
-#values = [967.67, 1190.89, 1079.75, 1349.19, 2328.91, 2504.28, 2873.83, 4764.87, 4349.29, 6458.30, 9907, 16297]
 colors = [
     "#F7464A", "#46BFBD", "#FDB45C", "#FEDCBA",
     "#ABCDEF", "#DDDDDD", "#ABCABC", "#4169E1",
@@ -26,7 +19,6 @@ def main():
     
     class ItemSelect(FlaskForm):
         items = mp_items.itemIDs.items()
-        #items = [(3075, "Adorned Empyrean Jewel"),(3070, "Bottled Wind")]
         selected = SelectField(u'Field name', choices = items, validators = [DataRequired()])
         submit = SubmitField('Submit')
     
